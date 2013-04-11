d8d3a5e1a981c013d5dd383ddf7d704d71d88f5e
Patrick Perini <pcperini@gmail.com>
2013-04-11 11:07:25 -0400

    add asynchronous processing

--- asana-status-board.py
+++ asana-status-board.py
@@ -4,20 +4,31 @@ import flask
 import jinja2
 import pyasana
 import datetime
+from libs.async import async
 
 # constants
 apiKey = "14THihfO.MBsmW5MxmCFrjFsNkFyivKd"
 workspaceID = "3799843448328"
 maximumNumberOfTasks = 5
+pageRefreshRate = 120 #seconds
 
 statusBoardTemplate = "asana-status-board.tmpl.html"
 
 # globals
 api = pyasana.Api(apiKey)
 flaskApp = flask.Flask(__name__)
+flaskApp.data = {
+    'recentTasks': [],
+    'isUpdatingRecentTasks': False
+}
 
 # functions
-def tasks():
+@async
+def updateRecentTasks():
+    if flaskApp.data['isUpdatingRecentTasks']:
+        return
+        
+    flaskApp.data['isUpdatingRecentTasks'] = True
     projects = api.get_projects(int(workspaceID))
 
     tasks = []
@@ -32,8 +43,9 @@ def tasks():
         task.assignee = api.get_user(int(task.assignee.id)) if task.assignee else None
         if task.assignee:
             task.assignee.avatar = "http://www.gravatar.com/avatar/%s" % (md5.new(task.assignee.email.lower()).hexdigest())
-    
-    return tasks
+            
+    flaskApp.data['recentTasks'] = tasks
+    flaskApp.data['isUpdatingRecentTasks'] = False
     
 # handlers
 @flaskApp.route("/")
@@ -42,7 +54,12 @@ def handleRoot():
 
 @flaskApp.route("/MegaBits")
 def handleMegaBitsTasks():
-    return flask.render_template(statusBoardTemplate, tasks = tasks())
+    updateRecentTasks()
+    return flask.render_template(
+        statusBoardTemplate,
+        tasks = flaskApp.data['recentTasks'],
+        refreshrate = pageRefreshRate
+    )
     
 # main
 if __name__ == "__main__":
